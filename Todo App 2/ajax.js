2b0405b7a88259c78342b2e95c634bc5b2d60a92
EL YAZIDI Mohamed <elyazidi.edu@gmail.com>
2021-04-12 18:18:00 +0000

    changes changes

--- Todo App 2/ajax.js
+++ Todo App 2/ajax.js
@@ -2,39 +2,43 @@ let Ajax = function () {
   this.xhr = new XMLHttpRequest();
 };
 
-Ajax.prototype.get = function (url, paramObject, action) {
-  let params = '';
-  let sep = '';
-  for (let p in paramObject) {
-    params = params + sep + p + '=' + paramObject[p];
-    sep = '&';
-  }
-  this.xhr.open('GET', url + '?' + params, true);
-  let ajax = this;
-  this.xhr.onreadystatechange = function () {
-    if (ajax.xhr.readyState === XMLHttpRequest.DONE) {
-      if (ajax.xhr.status === 200) {
-        ajax.text = ajax.xhr.responseText;
-        ajax.xml = ajax.xhr.responseXML;
-        action(ajax.xhr.responseText, ajax.xhr.responseXML);
-      }
+Ajax.prototype.makeRequest = function (method, url, paramObject) {
+  return new Promise((resolve, reject) => {
+    let ajax = this.xhr;
+    let params = '';
+    let sep = '';
+
+    for (let p in paramObject) {
+      params += sep + p + '=' + paramObject[p];
+      sep = '&';
     }
-  };
-  this.xhr.send();
+
+    ajax.open(method, url);
+    ajax.onload = function () {
+      if (ajax.status >= 200 && ajax.status < 300) {
+        resolve(ajax.response);
+      } else {
+        reject({
+          status: ajax.status,
+          statusText: ajax.statusText,
+        });
+      }
+    };
+    ajax.onerror = function () {
+      reject({
+        status: ajax.status,
+        statusText: ajax.statusText,
+      });
+    };
+
+    if (method === 'POST') {
+      ajax.setRequestHeader('Content-type', 'application/json');
+    }
+
+    ajax.send(params);
+  });
 };
 
-Ajax.html = function (id, content) {
-  document.getElementById(id).innerHTML = content;
-};
+let $ = new Ajax();
 
-Ajax.getHtml(id) = function (id) {
-  return document.getElementById(id);
-};
-
-Ajax.value = function (id, value) {
-  document.getElementById(id).value = value;
-};
-
-Ajax.getValue = function (id) {
-  return document.getElementById(id).value;
-};
+export { $ };
--- Todo App 2/app.js
+++ Todo App 2/app.js
@@ -1,62 +1,71 @@
+import { $ } from "./ajax.js";
+
 // Selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filter-todo');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', getTodos);
-document.addEventListener('click', function (e) {
-  const modalItem = document.querySelector('.modal');
+document.addEventListener("DOMContentLoaded", getTodos);
+document.addEventListener("click", function (e) {
+  const modalItem = document.querySelector(".modal");
   const clickedItem = e.target;
   if (clickedItem === modalItem) {
     modalItem.remove();
   }
 });
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event) {
   // Prevent form from submitting
   event.preventDefault();
-  const todoDiv = createTodoDiv({ isCompleted: false, task: todoInput.value });
+
+  const newTodo = {
+    "completed": false,
+    "task": todoInput.value,
+  };
+
+  // ADD TODO TO DATABASE
+  $.makeRequest("POST", "http://localhost:8080/api/v1/todo", newTodo);
+
+  // create TODO UI
+  const todoDiv = createTodoDiv(newTodo);
   todoList.appendChild(todoDiv);
 
-  // ADD TODO TO LOCAL STORAGE
-  saveLocalTodos(todoInput.value);
-
   // Clear Todo Input Value
-  todoInput.value = '';
+  todoInput.value = "";
 }
 
 function createTodoDiv(todo) {
   // Todo div
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add('todo');
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
   // Check if the task has been completed
-  if (todo.isCompleted) {
-    todoDiv.classList.add('completed');
+  if (todo.completed) {
+    todoDiv.classList.add("completed");
   }
 
   // Create LI
-  const newTodo = document.createElement('li');
+  const newTodo = document.createElement("li");
   newTodo.innerText = todo.task;
-  newTodo.classList.add('todo-item');
+  newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
   // CHECK MARK BUTTON
-  const completedButton = document.createElement('button');
+  const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add('complete-btn');
+  completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
 
   // TRASH BUTTON
-  const trashButton = document.createElement('button');
+  const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add('trash-btn');
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
 
   return todoDiv;
@@ -65,17 +74,17 @@ function createTodoDiv(todo) {
 function deleteCheck(e) {
   const item = e.target;
   // DELETE TODO
-  if (item.classList[0] === 'trash-btn') {
+  if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     // Show confirmation msg
-    createAndShowModal(todo);
+    //createAndShowModal(todo);
   }
 
   // CHECK MARK
-  if (item.classList[0] === 'complete-btn') {
+  if (item.classList[0] === "complete-btn") {
     const todo = item.parentElement;
-    changeTodoItemState(todo);
-    todo.classList.toggle('completed');
+    //changeTodoItemState(todo);
+    todo.classList.toggle("completed");
   }
 }
 
@@ -83,51 +92,34 @@ function filterTodo(e) {
   const todos = todoList.childNodes;
   todos.forEach(function (todo) {
     switch (e.target.value) {
-      case 'all':
-        todo.style.display = 'flex';
+      case "all":
+        todo.style.display = "flex";
         break;
-      case 'completed':
-        if (todo.classList.contains('completed')) {
-          todo.style.display = 'flex';
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
         } else {
-          todo.style.display = 'none';
+          todo.style.display = "none";
         }
         break;
-      case 'uncompleted':
-        if (!todo.classList.contains('completed')) {
-          todo.style.display = 'flex';
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
         } else {
-          todo.style.display = 'none';
+          todo.style.display = "none";
         }
         break;
     }
   });
 }
 
-function saveLocalTodos(todo) {
-  let todos = grabTodosFromLocalStorage();
-  const newTodo = {
-    isCompleted: false,
-    task: todo,
-  };
-  todos.push(newTodo);
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
-
-function grabTodosFromLocalStorage() {
-  const api = 'http://localhost:8080/api/v1/todo';
-  let todos;
-  if (localStorage.getItem('todos') === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-
-  return todos;
-}
-
-function getTodos() {
-  let todos = grabTodosFromLocalStorage();
+async function getTodos() {
+  let todos = await $.makeRequest(
+    "GET",
+    "http://localhost:8080/api/v1/todo",
+    {}
+  );
+  todos = JSON.parse(todos);
   todos.forEach(function (todo) {
     todoList.appendChild(createTodoDiv(todo));
   });
@@ -144,49 +136,40 @@ function changeTodoItemState(todo) {
       }
     }
   });
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
-
-function removeLocalTodos(todo) {
-  let todos = grabTodosFromLocalStorage();
-  let todoIndex = todo.children[0].innerText;
-  todos = todos.filter(function (todo) {
-    return !(todo.task === todoIndex);
-  });
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function createAndShowModal(todo) {
-  const divModal = document.createElement('div');
-  divModal.classList.add('modal');
+  const divModal = document.createElement("div");
+  divModal.classList.add("modal");
 
-  const modalContent = document.createElement('div');
-  modalContent.classList.add('content');
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("content");
 
-  const exclamationIcon = document.createElement('i');
-  exclamationIcon.classList.add('fas', 'fa-exclamation');
+  const exclamationIcon = document.createElement("i");
+  exclamationIcon.classList.add("fas", "fa-exclamation");
 
-  const modalText = document.createElement('p');
-  modalText.innerText = 'Are you sure, you want to delete this TODO ?';
+  const modalText = document.createElement("p");
+  modalText.innerText = "Are you sure, you want to delete this TODO ?";
 
-  const yesButton = document.createElement('button');
-  yesButton.classList.add('yes-btn');
+  const yesButton = document.createElement("button");
+  yesButton.classList.add("yes-btn");
   yesButton.innerText = "Yes, I'm sure";
-  yesButton.addEventListener('click', function () {
-    document.querySelector('.modal').remove();
+  yesButton.addEventListener("click", function () {
+    document.querySelector(".modal").remove();
     // Animation
     removeLocalTodos(todo);
-    todo.classList.add('fall');
-    todo.addEventListener('transitionend', function () {
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
       todo.remove();
     });
   });
 
-  const noButton = document.createElement('button');
-  noButton.classList.add('no-btn');
-  noButton.innerText = 'Cancel';
-  noButton.addEventListener('click', function () {
-    document.querySelector('.modal').remove();
+  const noButton = document.createElement("button");
+  noButton.classList.add("no-btn");
+  noButton.innerText = "Cancel";
+  noButton.addEventListener("click", function () {
+    document.querySelector(".modal").remove();
   });
 
   modalContent.appendChild(exclamationIcon);
